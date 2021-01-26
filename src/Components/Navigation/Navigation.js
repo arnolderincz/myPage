701451033b1984ffe06dac342224319766aed92a
Arnold Erincz <arnold.erincz@gmail.com>
2021-01-27 01:01:44 +0200

    added sections

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import {Link} from 'react-scroll';
+import Tilt from 'react-tilt';
+import './Navigation.css';
+import logoImg from '../../Images/the_A.png';
+
+
+const Navigation = ({className})  =>{
+
+    return(
+        <div className= {className.className}>
+
+        <nav>
+				<ul>
+					<Link
+						className="logo"
+						activeClass="active"
+						to="header"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}>
+						<Tilt className="logoTilt Tilt ml2 " options={{ max : 55 }} >
+							<img src= {logoImg} alt='logo'/>
+						</Tilt>
+					</Link>
+					
+					<Link
+						className="nav-item"
+						activeClass="active"
+						to="about"
+						spy={true}
+						smooth={true}
+						offset={-102}
+						duration={500}>
+						About me
+					</Link>
+					<Link
+						className="nav-item"
+						activeClass="active"
+						to="CS"
+						spy={true}
+						smooth={true}
+						offset={-102}
+						duration={500}>
+						Computer Science
+					</Link>
+					<li className="nav-item">
+					  <a  href="https://drive.google.com/file/d/1HsEnblqH52pkMVVBqnRoXJjdbIrcanX_/view?usp=sharing" target="_blank" rel='noreferrer'>CV</a>
+				  	</li>
+					<li className="nav-item">
+					  <a  href="https://drive.google.com/file/d/12XA7TakeQOiIFRoMKHsj0FGNlntxnEpM/view?usp=sharing" target="_blank" rel='noreferrer'>Resume</a>
+				  	</li>
+				  	<Link
+						className="nav-item"
+						activeClass="active"
+						to="contact"
+						spy={true}
+						smooth={true}
+						offset={-350}
+						duration={500}>
+						Contact me
+					</Link>
+				 </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navigation;
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const csCardsText  = [
+    {
+        "title" : "C / C++",
+        "text" : "These are the first languages that I studied and the ones that I've been studying the whole first year at UBB. But what do I know about them?  \n - create and use objects \n - use STL containers and iteratos \n - in ANSI C on Linux create subprocesses using fork and sending data between them \n - multithreading including synchronization with POSIX mutexes, semaphores, message queues and OpenMP"
+    },
+    {
+        "title" : "Java",
+        "text" : "I currently study Java at UBB. So far I know: \n - creating and using objects \n - using streams and higher order functions like map, filter, reduce \n - using AWT and Swing libraries \n - creating small java apps (for example I created a simple music player) \n- using action / item listeners \n - multithreading"
+    },
+    {
+        "title" : "JavaScript and Web Development",
+        "text" : "I started learning Javascript on my own from udemy courses after first year at UBB. By now I know: \n - using higher order functions like map, filter, reduce \n - the most important concepts like hoisting, closures, currying, polymorphism \n - make the difference between var, let, const \n - using React JS framework (I built this presentation website in React) \n - how to use Node JS but just as a beginner"
+    },
+    {
+        "title" : "Database Management",
+        "text" : "At Database class we use MS SQL, which is what I know the most. I'm familiar with: \n - how to design a database based on specifications \n - creating tables, views and temporary tables \n - writing different queries to find out some data \n - I also tried PostgresSQL and Node JS but I don't have experience in them"
+    },
+    {
+        "title" : "Haskell",
+        "text" : "In my opinion Haskell is a very powerful language, first time I wrote a little code in Haskell I was shocked how fast it can handle big numbers. I know: \n - the main concepts like currying, lazy evaluation \n - use functions like map, filter, until, fold \n - how it determines the types \n - to create and use custom types"
+    },
+    {
+        "title" : "Others",
+        "text" : "- I also have basic knowledge of prolog programing language \n - I practiced Git and Github  with this website and some other small ones \n - I have experience using Photoshop. I created a few event covers, logos, modified/enhanced pictures"
+    }
+]
